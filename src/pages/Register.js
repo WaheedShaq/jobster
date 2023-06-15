6ea0bef361767d00045fb8b621345632ec0f5abe
WaheedShaq <waheedshaqra@gmail.com>
2023-06-15 07:43:10 +0300

    finished the register page, added toastify for warning messages and initialized redux for user state manipulatiuon

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { Logo, FormRow } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
+import { toast } from 'react-toastify';
 
 const initialState = {
   name: '',
@@ -22,7 +23,7 @@ const Register = () => {
     e.preventDefault();
     const { name, email, password, isMember } = values;
     if (!email || !password(!isMember && !name)) {
-      console.log('please fill out all fields');
+      toast.error('please fill out all fields');
     }
   };
 
