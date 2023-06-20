f1d4a5289a5091ea750db068e0a7286fe5e3cfd5
WaheedShaq <waheedshaqra@gmail.com>
2023-06-20 08:14:26 +0300

    started on addJob functionality

---
+++ src/components/FormRowSelect.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import BigSidebar from './BigSidebar';
+import FormRow from './FormRow';
+import Logo from './Logo';
+import Navbar from './Navbar';
+import SmallSidebar from './SmallSidebar';
+
+const formRowSelect = ({ labelText, name, value, handleChange, list }) => {
+  return (
+    <div className='form-row'>
+      <label htmlFor={name} className='form-label'>
+        {labelText || name}
+      </label>
+      <select
+        name={name}
+        id={name}
+        value={value}
+        onChange={handleChange}
+        className='form-select'
+      >
+        {list.map((itemValue, index) => {
+          return (
+            <option key={index} value={itemValue}>
+              {itemValue}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default formRowSelect;
